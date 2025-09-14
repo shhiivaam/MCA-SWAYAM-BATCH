d61825462c859f185dac5b93adbbcea86f0efba5
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-09-14 19:43:40 +0530

    Update FunPermutation.cpp

---
+++ DAY_06/FunPermutation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+void solve() {
+    int n;
+    std::cin >> n;
+    std::vector<int> p(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> p[i];
+    }
+    for (int i = 0; i < n; ++i) {
+        std::cout << n + 1 - p[i] << (i == n - 1 ? "" : " ");
+    }
+    std::cout << "\n";
+}
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int t;
+    std::cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
