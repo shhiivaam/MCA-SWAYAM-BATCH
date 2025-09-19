1980ec40048b5436444e903c84f850f9176d97d6
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-09-19 11:24:20 +0530

    Create Shortest Routes I.cpp

---
+++ DAY_11/Shortest Routes I.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll INF = 1e18;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int n, m;
+    cin >> n >> m;
+ 
+    vector<vector<pair<int,int>>> adj(n+1);
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj[a].push_back({b, c});
+    }
+ 
+    vector<ll> dist(n+1, INF);
+    dist[1] = 0;
+ 
+    priority_queue<pair<ll,int>, vector<pair<ll,int>>, greater<>> pq;
+    pq.push({0, 1});
+ 
+    while (!pq.empty()) {
+        auto [d, u] = pq.top();
+        pq.pop();
+        if (d != dist[u]) continue;
+ 
+        for (auto [v, w] : adj[u]) {
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+ 
+    for (int i = 1; i <= n; i++) {
+        cout << dist[i] << " ";
+    }
+    cout << "\n";
+}
