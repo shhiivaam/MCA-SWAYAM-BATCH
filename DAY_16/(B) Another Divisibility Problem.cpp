53c0f86bfbe633a44ae62af343331073d1de9812
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-10-03 11:04:49 +0530

    Create (B) Another Divisibility Problem.cpp

---
+++ DAY_16/(B) Another Divisibility Problem.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t; cin >> t;
+    while (t--) {
+       int x; cin>>x;
+
+       long long y = 2 * x;
+       cout<<y<<endl;
+    }
+}
