5adb69bc02b01a223f5de239f7359bb9ac6ef908
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-11-04 11:59:58 +0530

    Create Odd Sum.cpp

---
+++ MixedPractice/Odd Sum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve(){
+
+    ll N;
+    cin >> N;
+    
+    cout << 1 + (N-2)*(N-1) << "\n";
+
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int tt;
+
+	cin >> tt;
+	while(tt--){
+		solve();
+	}
+}
